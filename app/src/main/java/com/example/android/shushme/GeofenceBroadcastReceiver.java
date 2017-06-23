24e1fbc676594c07cec5eaf6b5d9ab6ef36bf656
Ehbraheem <ehbraheem@gmail.com>
2017-06-22 20:43:48 -0700

    Almost

--- app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
+++ app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
@@ -16,10 +16,14 @@ package com.example.android.shushme;
 * limitations under the License.
 */
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.util.Log;
+
+import com.google.android.gms.location.GeofencingEvent;
 
 public class GeofenceBroadcastReceiver extends BroadcastReceiver {
 
@@ -38,11 +42,24 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
         Log.i(TAG, "onReceive called");
         // TODO (4) Use GeofencingEvent.fromIntent to retrieve the GeofencingEvent that caused the transition
 
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+
         // TODO (5) Call getGeofenceTransition to get the transition type and use AudioManager to set the
         // phone ringer mode based on the transition type. Feel free to create a helper method (setRingerMode)
+
 
         // TODO (6) Show a notification to alert the user that the ringer mode has changed.
         // Feel free to create a helper method (sendNotification)
 
+    }
+
+    private void setRingerMode(Context context, int mode) {
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT < 24 ||
+                (android.os.Build.VERSION.SDK_INT >= 24 && nm.isNotificationPolicyAccessGranted())) {
+            AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            audioManager.setRingerMode(mode);
+        }
     }
 }
